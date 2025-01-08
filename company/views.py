e4a1407254c1374895651f825ce8b14fd359f3ec
Kashish <kashishgadhiya24@gmail.com>
2025-01-08 12:45:16 +0530

    check

--- company/views.py
+++ company/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Company
 
-# Create your views here.
+
 @login_required
 def register_company(request):
 
